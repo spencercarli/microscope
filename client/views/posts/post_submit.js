51dcc33a665f82241defecd34688c74321377a9d
Spencer Carli <spencercarli@gmail.com>
2013-10-24 11:33:50 -0400

    Use a method to submit a post

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -1,13 +1,18 @@
 Template.postSubmit.events({ 
 	'submit form': function(event) {
-	event.preventDefault();
+		event.preventDefault();
 
-	var post = {
-		url: $(event.target).find('[name=url]').val(),
-		title: $(event.target).find('[name=title]').val(), 
-		message: $(event.target).find('[name=message]').val()
+		var post = {
+			url: $(event.target).find('[name=url]').val(),
+			title: $(event.target).find('[name=title]').val(), 
+			message: $(event.target).find('[name=message]').val()
+		}
+
+		Meteor.call('post', post, function(error, id){
+			if ( error )
+				return alert(error.reason);
+
+			Meteor.Router.to('postPage', id); 
+		});
 	}
-	
-	post._id = Posts.insert(post);
-	Meteor.Router.to('postPage', post); }
 });
