3f96d066f071c56bca31ccf5980d97f477bc9a9d
Spencer Carli <spencercarli@gmail.com>
2013-11-06 14:49:59 -0500

    Use the paginated subscription to step through posts.

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,23 +1,21 @@
-var postsData = [
-	{
-		title: 'Introducing Telescope',
-		author: 'Sacha Greif',
-		url: 'http://sachagreif.com/introducing-telescope/'
-	},
-	{
-		title: 'Meteor',
-		author: 'Tom Coleman', 
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book', 
-		author: 'Tom Coleman',
-		url: 'http://themeteorbook.com'
-	}
-]
-
 Template.postsList.helpers({ 
 	posts: function() {
-		return Posts.find({}, {sort: {submitted: -1}}); 
+		return Posts.find({}, {sort: {submitted: -1}, limit: postsHandle.limit() }); 
+	}, 
+
+	postsReady: function() {
+		return ! postsHandle.loading(); 
+	},
+
+	allPostsLoaded: function() {
+		return ! postsHandle.loading() 
+		&& Posts.find().count() < postsHandle.loaded(); 
+	}
+});
+
+Template.postsList.events({
+	'click .load-more': function(event) {
+		event.preventDefault();
+		postsHandle.loadNextPage(); 
 	}
 });
