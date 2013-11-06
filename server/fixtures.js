f5fc9bef69201001fc99ea1957967244c253eca2
Spencer Carli <spencercarli@gmail.com>
2013-11-06 13:51:13 -0500

    Denormalized the number of comments into the post.

--- server/fixtures.js
+++ server/fixtures.js
@@ -19,7 +19,8 @@ if (Posts.find().count() === 0) { var now = new Date().getTime();
 		userId: sacha._id,
 		author: sacha.profile.name,
 		url: 'http://sachagreif.com/introducing-telescope/',
-		submitted: now - 7 * 3600 * 1000 
+		submitted: now - 7 * 3600 * 1000,
+		commentsCount: 2
 	});
 
 	Comments.insert({
@@ -40,13 +41,15 @@ if (Posts.find().count() === 0) { var now = new Date().getTime();
 		title: 'Meteor',
 		userId: tom._id,
 		author: tom.profile.name,
-		url: 'http://meteor.com', submitted: now - 10 * 3600 * 1000
+		url: 'http://meteor.com', submitted: now - 10 * 3600 * 1000,
+		commentsCount: 0
 	});
 
 	Posts.insert({
 		title: 'The Meteor Book',
 		userId: tom._id,
 		author: tom.profile.name,
-		url: 'http://themeteorbook.com', submitted: now - 12 * 3600 * 1000
+		url: 'http://themeteorbook.com', submitted: now - 12 * 3600 * 1000,
+		commentsCount: 0
 	}); 
 }
